0b3e033dcc5bcf8899df21b8b4117f178cc2383f
Lucien Aibel <lucien.aibel@mymail.champlain.edu>
2025-02-05 21:53:58 -0500

    Hills they are a rollin'

---
+++ project_octopus/worldrenderer.gd
@@ -0,0 +1,20 @@
+extends Node
+
+const GRID_SIZE = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	randomize()
+	var noisegen = FastNoiseLite.new()
+	noisegen.seed = randi()
+	var tileScene = load("res://tile.tscn")
+	for x in range(GRID_SIZE):
+		for z in range(GRID_SIZE):
+			var newTile = tileScene.instantiate()
+			add_child(newTile)
+			newTile.position = Vector3(x - (GRID_SIZE / 2), 0.0, z - (GRID_SIZE / 2))
+			newTile.scale = Vector3(1.0, (noisegen.get_noise_2d(x,z) * 20) + 1.0, 1.0)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
