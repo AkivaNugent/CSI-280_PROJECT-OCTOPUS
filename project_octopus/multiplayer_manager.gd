b7eaa5654eacf64ec661fa5244a7171dea330e85
Glen Kelley <glen.kelley@mymail.champlain.edu>
2025-03-05 00:08:23 -0500

    I got a stable connection of two clients to the dedicated server.
    I have a dedicated server and I can connect to it! I would like some help in putting both players in the same game though... Also, we could get rid of the host functions entirely now that we have a dedicated server.

--- project_octopus/multiplayer_manager.gd
+++ project_octopus/multiplayer_manager.gd
@@ -4,6 +4,10 @@ const SERVER_PORT = 8080
 const SERVER_IP = "127.0.0.1"
 
 
+func _ready():
+	if OS.has_feature("dedicated_server"):
+		print("Starting dedicated server...")
+		MultiplayerManager.become_host()
 # will likeley be called using a button press
 func become_host():
 	print("Starting host!")
@@ -19,7 +23,10 @@ func become_host():
 	
 	#detect a player is added to the server so we can add them to the game
 	get_tree().get_multiplayer().peer_connected.connect(_add_player_to_game)
-	get_tree().get_multiplayer().peer_connected.connect(_del_player)
+	get_tree().get_multiplayer().peer_disconnected.connect(_del_player)
+	
+	if not OS.has_feature("dedicated_server"):
+		_add_player_to_game(1)
 
 func join_as_player_2():
 	print("Player 2 joining")
@@ -32,10 +39,16 @@ func join_as_player_2():
 		return
 	
 	get_tree().get_multiplayer().multiplayer_peer = client_peer
+	var connected = get_tree().get_multiplayer().is_connected("peer_connected", Callable(self, "_add_player_to_game"))
+	if not connected:
+		get_tree().get_multiplayer().peer_connected.connect(_add_player_to_game)
+	var disconnected = get_tree().get_multiplayer().is_connected("peer_disconnected", Callable(self, "_del_player"))
+	if not disconnected:
+		get_tree().get_multiplayer().peer_disconnected.connect(_del_player)
 	
-
 func _add_player_to_game(id: int):
 	print("Player %s joined the game!" % id)
+	
 
 func _del_player(id: int):
 	print("Player %s left the game!" % id)
