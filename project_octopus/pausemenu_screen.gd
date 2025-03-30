97d2e5fe3b3c8b6f30e4a6e91196ffe55c319040
deris.o2024 <deris.omalley@mymail.champlain.edu>
2025-03-30 11:29:01 -0400

    Added Volume Settings

--- project_octopus/pausemenu_screen.gd
+++ project_octopus/pausemenu_screen.gd
@@ -5,21 +5,27 @@ extends Control
 func _ready() -> void:
 	pause_menu.visible = false
 
+	# Init volume values
+	$musicMenu/master_hslider.value = db_to_linear(AudioServer.get_bus_volume_db(0))
+	$musicMenu/music_hslider.value = db_to_linear(AudioServer.get_bus_volume_db(1))
+	$musicMenu/sfx_hslider.value = db_to_linear(AudioServer.get_bus_volume_db(2))
+
 func pause():
 	get_tree().paused = true
 	pause_menu.visible = true
 	
 func resume():
+	AudioServer.set_bus_volume_db(0, linear_to_db($musicMenu/master_hslider.value))
+	AudioServer.set_bus_volume_db(1, linear_to_db($musicMenu/music_hslider.value))
+	AudioServer.set_bus_volume_db(2, linear_to_db($musicMenu/sfx_hslider.value))
 	get_tree().paused = false
 	pause_menu.visible = false
 	
 func togglePauseMenu():
 	if Input.is_action_just_pressed("pause") && !get_tree().paused:
 		pause()
-		print("pause")
 	elif Input.is_action_just_pressed("pause") && get_tree().paused:
 		resume()
-		print("unpause")
 
 
 func _on_resume_pressed() -> void:
@@ -37,3 +43,15 @@ func _on_quit_pressed() -> void:
 
 func _process(delta: float) -> void:
 	togglePauseMenu()
+
+# Release on Volume Sliders
+func _on_master_hslider_mouse_exited() -> void:
+	release_focus()
+
+
+func _on_music_hslider_mouse_exited() -> void:
+	release_focus()
+
+
+func _on_sfx_hslider_mouse_exited() -> void:
+	release_focus()
